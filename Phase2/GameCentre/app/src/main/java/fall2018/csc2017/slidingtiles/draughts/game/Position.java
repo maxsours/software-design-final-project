3ae974bafc6c14d1f1ab15e0c7fe3d8e86139595
Luka Trkla <luka.trkla@mail.utoronto.ca>
2018-11-29 17:22:21 -0500

    Added java documentation to the entirety of Position.java class.

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/draughts/game/Position.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/draughts/game/Position.java
@@ -1,22 +1,41 @@
 package fall2018.csc2017.slidingtiles.draughts.game;
 
 import java.io.Serializable;
+
 /**
+ * A position for a checkers game move.
  * Adapted on 2018/11/15 from an openly available applet by Greg Tour:
  * https://github.com/gregtour/CheckersAndroid
  */
-/**
- * Created by Greg on 8/6/2017.
- */ // x y coordinate
 public class Position implements Serializable {
+
+    /**
+     * The position's row position.
+     */
     public final int x;
+
+    /**
+     * The position's column position.
+     */
     public final int y;
 
+    /**
+     * Creates a checkers game position.
+     *
+     * @param x the position's row position.
+     * @param y the position's column position.
+     */
     public Position(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Adds to's position to the current position.
+     *
+     * @param to the position to be added to the current position.
+     * @return the summed position of to and the current position.
+     */
     public Position plus(Position to) {
         return new Position(x + to.x, y + to.y);
     }
